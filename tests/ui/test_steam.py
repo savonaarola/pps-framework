f0a0fe340966b0c6c591a41f393f36d5b9411138
savonaarola <ibn122123@gmail.com>
2025-10-06 16:02:16 +0300

    Test if online amount more than ingame

---
+++ tests/ui/test_steam.py
@@ -0,0 +1,15 @@
+from pages.steam_page import SteamAboutPage,SteamMainPage
+import pytest
+
+
+@pytest.mark.ui
+def test_compare_amount_online_vs_ingame(driver):
+    driver.get('https://store.steampowered.com/')
+    main_page = SteamMainPage(driver)
+    main_page.click_about_button()
+    
+    about_page = SteamAboutPage(driver)
+    online_amount = about_page.get_amount_players_online()
+    ingame_amount = about_page.get_amount_players_ingame()
+    
+    assert online_amount > ingame_amount
